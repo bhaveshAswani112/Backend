49fec2d02e2fbdb7dcb7209438995c1487323634
bhaveshAswani112 <aswanib133@gmail.com>
2024-01-06 19:26:50 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.models.js"
 import { uploadOnCloudinary } from "../utils/Cloudinary.js";
 import { ApiResponse } from "../utils/APiResponse.js";
-
+import fs from "fs"
 
 const registerUser = asyncHandler(async (req,res)=>{
     // will take data from frontend
@@ -15,34 +15,65 @@ const registerUser = asyncHandler(async (req,res)=>{
     // remove password and refresh token from response
     // check for user creation
     // return response
-
+    console.log("I am body  ",req.body)
     const {fullName,username,email,password} = req.body
     if(
         [fullName,username,email,password].some((field) => field?.trim() === "")
     ){
-        ApiError(400,"All fields are required")
+        if(req.files.avatar && req.files.avatar.length > 0){
+            fs.unlinkSync(req.files.avatar[0].path)
+        }
+        if(req.files.coverImage && req.files.coverImage.length > 0){
+            fs.unlinkSync(req.files.coverImage[0].path)
+        }
+        throw new ApiError(400,"All fields are required")
     }
+    console.log(fullName)
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or : [{ username } , { email }]
     }
     )
     if(existedUser){
+        if(req.files.avatar && req.files.avatar.length > 0){
+            fs.unlinkSync(req.files.avatar[0].path)
+        }
+        if(req.files.coverImage && req.files.coverImage.length > 0){
+            fs.unlinkSync(req.files.coverImage[0].path)
+        }
         throw new ApiError(409,"User with mail or username already exists")
     }
-
-    const avatar_local_path = req.files?.avatar[0]?.path
-    const cover_local_path = req.files?.coverImage[0]?.path
-    if(!avatar_local_path)throw new ApiError(400,"Avatar is required")
+    
+    console.log("I am req.files  ",req.files)
+    let avatar_local_path = undefined
+    if(req.files.avatar){
+        avatar_local_path =  req.files?.avatar[0].path
+    }
+    
+    if(!avatar_local_path){
+        if(req.files.avatar && req.files.avatar.length > 0){
+            fs.unlinkSync(req.files.avatar[0].path)
+        }
+        if(req.files.coverImage && req.files.coverImage.length > 0){
+            fs.unlinkSync(req.files.coverImage[0].path)
+        }
+        throw new ApiError(400,"Avatar is required")
+    }
+    let cover_local_path = undefined
+    if(req.files.coverImage){
+        cover_local_path =  req.files?.coverImage[0].path
+    }
+    
+    
 
     const avatar = await uploadOnCloudinary(avatar_local_path)
     const coverImage = await uploadOnCloudinary(cover_local_path)
 
-    if(!avatar)throw new ApiError(400,"Avatar is required")
+    if(!avatar)throw new ApiError(400,"Avatar is required for cloudinary")
 
     const user = await User.create({
         fullName : fullName,
-        username : username.toLwerCase(),
+        username : username.toLowerCase(),
         email : email,
         password : password,
         avatar : avatar.url,
--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -3,7 +3,7 @@ import fs from "fs"
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret:  process.env.CLOUDINARY_API_SECRET
+  api_secret:  process.env.CLOUDINARY_SECRET
 });
 
 const uploadOnCloudinary = async (filePath) => {
@@ -12,7 +12,9 @@ const uploadOnCloudinary = async (filePath) => {
         const response = await cloudinary.uploader.upload(filePath,{
             resource_type : 'auto'
         })
-        console.log("File has been uploaded successfully!!!"  ,response.url)
+        // console.log("File has been uploaded successfully!!!"  ,response.url)
+        fs.unlinkSync(filePath)
+        console.log("I am cloudinary response  ",response)
         return response
         
     } catch (error) {
